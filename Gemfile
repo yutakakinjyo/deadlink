e443d6cb613f4298c7894bc9bf4846e24a7199c7
yutakakinjyo <yutakakinjyo@gmail.com>
2015-11-13 22:35:15 +0900

    remove coverage config

--- Gemfile
+++ Gemfile
@@ -2,4 +2,3 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in deadlink.gemspec
 gemspec
-gem "codeclimate-test-reporter", group: :test, require: nil
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,4 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'deadlink'
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
 
 require 'minitest/autorun'
