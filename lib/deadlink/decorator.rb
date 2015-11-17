19716bed450b928d27d46a8e5e6400b4567ebedc
yutakakinjyo <yutakakinjyo@gmail.com>
2015-11-17 23:43:05 +0900

    divide method

--- lib/deadlink/decorator.rb
+++ lib/deadlink/decorator.rb
@@ -2,10 +2,21 @@ module Deadlink
   class Decorator
     def self.print_info(path,opts)
       if opts['p']
-        puts '+' + path.index.to_s + " " + path.cur_file_path
+        for_editor(path)
       else
-        puts path.link + ' in ' + path.cur_file_path + ' line: ' + path.index.to_s
+        default(path)
       end
     end
+
+    private
+    
+    def self.for_editor(path)
+      puts '+' + path.index.to_s + " " + path.cur_file_path
+    end
+
+    def self.default(path)
+      puts path.link + ' in ' + path.cur_file_path + ' line: ' + path.index.to_s
+    end
+    
   end
 end
