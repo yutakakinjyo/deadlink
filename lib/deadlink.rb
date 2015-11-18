9f069316bed1ed0898ae022fc5d5a870f436b29c
yutakakinjyo <yutakakinjyo@gmail.com>
2015-11-19 01:11:14 +0900

    ore cool display

--- lib/deadlink.rb
+++ lib/deadlink.rb
@@ -11,13 +11,10 @@ module Deadlink
     opts = ARGV.getopts('','p')
     target_dir = ARGV[0]
 
-    begin
-      scanner = Scanner.new(target_dir)
-      files = scanner.md_files
-      paths = scanner.paths(files)
-      paths.print_deadlinks(opts)
-    rescue => e
-      e.message
-    end
+
+    scanner = Scanner.new(target_dir)
+    files = scanner.md_files
+    paths = scanner.paths(files)
+    paths.print_deadlinks(opts)
   end
 end
