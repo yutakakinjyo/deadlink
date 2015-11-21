f7aaef32f64d421b5e5c9b7e7c24a10c1058aea8
yutakakinjyo <yutakakinjyo@gmail.com>
2015-11-21 13:39:38 +0900

    header title cpature test

--- test/get_headers_test.rb
+++ test/get_headers_test.rb
@@ -18,7 +18,6 @@ class DeadlinkTest < Minitest::Test
   def test_get_single_atx_headers
 
     File.open(@file_path, 'a') do |f|
-      f.puts "[dummy](mdfile.md#header1)"
       f.puts "# header1"
     end
     
@@ -27,12 +26,12 @@ class DeadlinkTest < Minitest::Test
     headers = scanner.headers(files[0])
 
     assert_equal 1, headers.count
+    assert_equal "header1", headers[0]
   end
 
   def test_get_two_atx_headers
 
     File.open(@file_path, 'a') do |f|
-      f.puts "[dummy](mdfile.md#header1)"
       f.puts "## header1"
     end
 
@@ -40,13 +39,13 @@ class DeadlinkTest < Minitest::Test
     files = scanner.md_files
     headers = scanner.headers(files[0])
 
-    assert_equal 1, headers.count    
+    assert_equal 1, headers.count
+    assert_equal "header1", headers[0]
   end
 
   def test_bad_synntax_atx_headers
 
     File.open(@file_path, 'a') do |f|
-      f.puts "[dummy](mdfile.md#header1)"
       f.puts "#header1"
     end
     
@@ -55,13 +54,28 @@ class DeadlinkTest < Minitest::Test
     headers = scanner.headers(files[0])
 
     assert_equal 0, headers.count
-    
   end
+
+  def test_two_sentence_atx_headers
+
+    File.open(@file_path, 'a') do |f|
+      f.puts "# header1"
+      f.puts "## header2"
+    end
+    
+    scanner = Deadlink::Scanner.new(nil)
+    files = scanner.md_files
+    headers = scanner.headers(files[0])
+
+    assert_equal 2, headers.count
+    assert_equal "header1", headers[0]
+    assert_equal "header2", headers[1]
+  end
+
 
   def test_many_atx_headers
 
     File.open(@file_path, 'a') do |f|
-      f.puts "[dummy](mdfile.md#header1)"
       f.puts "####### header1"
     end
     
@@ -82,7 +96,24 @@ class DeadlinkTest < Minitest::Test
     files = scanner.md_files
     headers = scanner.headers(files[0])
     assert_equal 0, headers.count
+  end
+
+  def test_two_file_atx_headers
+
+    skip();
     
+    File.open(@file_path, 'a') do |f|
+      f.puts "# file1 header"
+    end
+
+    File.open("mdifle2.md", 'a') do |f|
+      f.puts "# file2 header"
+    end
+    
+    scanner = Deadlink::Scanner.new(nil)
+    files = scanner.md_files
+    headers = scanner.headers(files[0])
+    assert_equal 0, headers.count
   end
   
 end
