b0251c744fa4af13c5539e88c5536bfa669138f0
yutakakinjyo <yutakakinjyo@gmail.com>
2015-11-21 13:10:18 +0900

    collect common test code to before/after function

--- test/get_headers_test.rb
+++ test/get_headers_test.rb
@@ -3,126 +3,85 @@ require 'fakefs/safe'
 
 class DeadlinkTest < Minitest::Test
 
+  def setup
+    FakeFS.activate!
+    FileUtils.mkdir_p 'git_repo/.git'
+    FileUtils.cd 'git_repo'
+    @file_path = File.join('mdfile.md')
+  end
+
+  def teardown
+    FakeFS::FileSystem.clear
+    FakeFS.deactivate!
+  end
+  
   def test_get_single_atx_headers
 
-    FakeFS.activate!
-
-    FileUtils.mkdir_p 'git_repo/.git'
-    file_path = File.join('git_repo', 'mdfile.md')
-
-    File.open(file_path, 'a') do |f|
+    File.open(@file_path, 'a') do |f|
       f.puts "[dummy](mdfile.md#header1)"
       f.puts "# header1"
     end
     
-    FileUtils.cd 'git_repo'
-
     scanner = Deadlink::Scanner.new(nil)
     files = scanner.md_files
     headers = scanner.headers(files[0])
 
     assert_equal 1, headers.count
-
-    FakeFS::FileSystem.clear
-    FakeFS.deactivate!
-    
   end
 
   def test_get_two_atx_headers
 
-    FakeFS.activate!
-
-    FileUtils.mkdir_p 'git_repo/.git'
-    file_path = File.join('git_repo', 'mdfile.md')
-
-    File.open(file_path, 'a') do |f|
+    File.open(@file_path, 'a') do |f|
       f.puts "[dummy](mdfile.md#header1)"
       f.puts "## header1"
     end
-    
-    FileUtils.cd 'git_repo'
 
     scanner = Deadlink::Scanner.new(nil)
     files = scanner.md_files
     headers = scanner.headers(files[0])
 
-    assert_equal 1, headers.count
-
-    FakeFS::FileSystem.clear
-    FakeFS.deactivate!
-    
+    assert_equal 1, headers.count    
   end
 
-  def test_bad__synntax_atx_headers
+  def test_bad_synntax_atx_headers
 
-    FakeFS.activate!
-
-    FileUtils.mkdir_p 'git_repo/.git'
-    file_path = File.join('git_repo', 'mdfile.md')
-
-    File.open(file_path, 'a') do |f|
+    File.open(@file_path, 'a') do |f|
       f.puts "[dummy](mdfile.md#header1)"
       f.puts "#header1"
     end
     
-    FileUtils.cd 'git_repo'
-
     scanner = Deadlink::Scanner.new(nil)
     files = scanner.md_files
     headers = scanner.headers(files[0])
 
     assert_equal 0, headers.count
-
-    FakeFS::FileSystem.clear
-    FakeFS.deactivate!
     
   end
 
   def test_many_atx_headers
 
-    FakeFS.activate!
-
-    FileUtils.mkdir_p 'git_repo/.git'
-    file_path = File.join('git_repo', 'mdfile.md')
-
-    File.open(file_path, 'a') do |f|
+    File.open(@file_path, 'a') do |f|
       f.puts "[dummy](mdfile.md#header1)"
-      f.puts "######## header1"
+      f.puts "####### header1"
     end
     
-    FileUtils.cd 'git_repo'
-
     scanner = Deadlink::Scanner.new(nil)
     files = scanner.md_files
     headers = scanner.headers(files[0])
 
     assert_equal 0, headers.count
-
-    FakeFS::FileSystem.clear
-    FakeFS.deactivate!
-    
   end
   
   def test_get_nothing_atx_headers
 
-    FakeFS.activate!
-
-    FileUtils.mkdir_p 'git_repo/.git'
-    file_path = File.join('git_repo', 'mdfile.md')
-
-    File.open(file_path, 'a') do |f|
+    File.open(@file_path, 'a') do |f|
       f.puts "[dummy](mdfile.md#header1)"
     end
     
-    FileUtils.cd 'git_repo'
-
     scanner = Deadlink::Scanner.new(nil)
     files = scanner.md_files
     headers = scanner.headers(files[0])
     assert_equal 0, headers.count
-
-    FakeFS::FileSystem.clear
-    FakeFS.deactivate!
     
   end
   
