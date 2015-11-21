7b1ac81dc8d67752f2f7a5463951e8c508753fe7
yutakakinjyo <yutakakinjyo@gmail.com>
2015-11-21 14:34:39 +0900

    use self func insted of setup and teardown. I shoul reseach more smart way

--- test/get_headers_test.rb
+++ test/get_headers_test.rb
@@ -3,21 +3,23 @@ require 'fakefs/safe'
 
 class DeadlinkTest < Minitest::Test
 
-  def setup
-    FakeFS.activate!
+  
+  def ready_file
     FileUtils.mkdir_p 'git_repo/.git'
     FileUtils.cd 'git_repo'
-    @file_path = File.join('mdfile.md')
+    FakeFS.activate!
   end
 
-  def teardown
+  def close_file
     FakeFS::FileSystem.clear
     FakeFS.deactivate!
   end
   
   def test_get_single_atx_headers
 
-    File.open(@file_path, 'a') do |f|
+    ready_file
+    
+    File.open('mdfils.md', 'a') do |f|
       f.puts "# header1"
     end
     
@@ -27,11 +29,15 @@ class DeadlinkTest < Minitest::Test
 
     assert_equal 1, headers.count
     assert_equal "header1", headers[0]
+
+    close_file
   end
 
   def test_get_two_atx_headers
 
-    File.open(@file_path, 'a') do |f|
+    ready_file
+    
+    File.open('mdfils.md', 'a') do |f|
       f.puts "## header1"
     end
 
@@ -41,11 +47,15 @@ class DeadlinkTest < Minitest::Test
 
     assert_equal 1, headers.count
     assert_equal "header1", headers[0]
+
+    close_file
   end
 
   def test_bad_synntax_atx_headers
 
-    File.open(@file_path, 'a') do |f|
+    ready_file
+    
+    File.open('mdfils.md', 'a') do |f|
       f.puts "#header1"
     end
     
@@ -54,11 +64,15 @@ class DeadlinkTest < Minitest::Test
     headers = scanner.headers(files[0])
 
     assert_equal 0, headers.count
+
+    close_file
   end
 
   def test_two_sentence_atx_headers
 
-    File.open(@file_path, 'a') do |f|
+    ready_file
+
+    File.open('mdfils.md', 'a') do |f|
       f.puts "# header1"
       f.puts "## header2"
     end
@@ -70,12 +84,16 @@ class DeadlinkTest < Minitest::Test
     assert_equal 2, headers.count
     assert_equal "header1", headers[0]
     assert_equal "header2", headers[1]
+
+    close_file
   end
 
 
   def test_many_atx_headers
 
-    File.open(@file_path, 'a') do |f|
+    ready_file
+    
+    File.open('mdfils.md', 'a') do |f|
       f.puts "####### header1"
     end
     
@@ -84,11 +102,15 @@ class DeadlinkTest < Minitest::Test
     headers = scanner.headers(files[0])
 
     assert_equal 0, headers.count
+
+    close_file
   end
   
   def test_get_nothing_atx_headers
 
-    File.open(@file_path, 'a') do |f|
+    ready_file
+    
+    File.open('mdfils.md', 'a') do |f|
       f.puts "[dummy](mdfile.md#header1)"
     end
     
@@ -96,13 +118,15 @@ class DeadlinkTest < Minitest::Test
     files = scanner.md_files
     headers = scanner.headers(files[0])
     assert_equal 0, headers.count
+
+    close_file
   end
 
   def test_two_file_atx_headers
 
     skip();
     
-    File.open(@file_path, 'a') do |f|
+    File.open('mdfils.md', 'a') do |f|
       f.puts "# file1 header"
     end
 
