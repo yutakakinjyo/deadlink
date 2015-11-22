44458830e3383bf5c97bd6c5b52a24ec2af457c0
yutakakinjyo <yutakakinjyo@gmail.com>
2015-11-22 20:24:22 +0900

    refactor

--- lib/deadlink/md_file.rb
+++ lib/deadlink/md_file.rb
@@ -15,14 +15,22 @@ module Deadlink
       @link_paths = []
       File.open(file_path) do |f|
         f.each_with_index do |line,index|
-          if line =~ /^\#{1,6} +(?<header>.+)/ # capture sharp header part
+          if line =~ heading_pattern # capture sharp header part
             @headers.push Regexp.last_match[:header]
-          end
-          line.scan /\[[^\]]*\]\(([^)]+)\)/ do |link|
-            @link_paths.push Path.new(file_path, link[0], index + 1, repo_root)
+          elsif line =~ link_pattern # capthure link path part
+            @link_paths.push Path.new(file_path, Regexp.last_match[:link], index + 1, repo_root)
           end
         end
       end
     end
+
+    def heading_pattern
+      /^\#{1,6} +(?<header>.+)/
+    end
+
+    def link_pattern
+      /\[[^\]]*\]\((?<link>[^)]+)\)/
+    end
+    
   end
 end
