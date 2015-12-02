120762bacba1ca6fa847dc96c20eff7f3404b73e
yutakakinjyo <yutakakinjyo@gmail.com>
2015-12-03 01:23:55 +0900

    yield

--- lib/deadlink/md_file.rb
+++ lib/deadlink/md_file.rb
@@ -7,27 +7,19 @@ module Deadlink
       @path = file_path
       @headers = []
       @link_paths = []
-      init(repo_root)
+      attribute(repo_root)
     end
     
     private
     
-    def init(repo_root)
+    def attribute(repo_root)
       File.open(@path) do |f|
         prev_line = nil
         f.each_with_index do |line,index|
-          sharp_header(line)
-          link(line, index, repo_root)
-          under_line_header(line, prev_line)
+          sharp_header(line) { |header| @headers.push header }
+          link(line, index, repo_root) { |path| @link_paths.push path }
+          under_line_header(line, prev_line) { |header| @headers.push header }
           prev_line = line
-        end
-      end
-    end
-
-    def under_line_header(line, prev_line)
-      if line =~ /^[-]+$|^[=]+$/
-        unless prev_line.nil?
-          @headers.push prev_line.chomp
         end
       end
     end
@@ -35,13 +27,21 @@ module Deadlink
     def sharp_header(line)
       if line =~ heading_pattern # capture sharp header part
         header = Regexp.last_match[:header].downcase.rstrip.gsub(/\s+/," ")
-        @headers.push header.gsub(" ", "-")
+        yield header.gsub(" ", "-")
+      end
+    end
+    
+    def under_line_header(line, prev_line)
+      if line =~ /^[-]+$|^[=]+$/
+        unless prev_line.nil?
+          yield prev_line.chomp
+        end
       end
     end
 
     def link(line, index, repo_root)
       line.scan link_pattern do |link| # capthure links path part
-        @link_paths.push Path.new(@path, link[0].rstrip, index + 1, repo_root)
+        yield Path.new(@path, link[0].rstrip, index + 1, repo_root)
       end
     end
 
