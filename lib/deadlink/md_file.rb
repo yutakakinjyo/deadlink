8c730e001c67d7158f365ec7e1a520014f4fa22b
yutakakinjyo <yutakakinjyo@gmail.com>
2015-12-03 00:36:54 +0900

    divided method

--- lib/deadlink/md_file.rb
+++ lib/deadlink/md_file.rb
@@ -16,22 +16,30 @@ module Deadlink
       File.open(@path) do |f|
         prev_line = nil
         f.each_with_index do |line,index|
-          attribute(line, index, repo_root)
-          if line =~ /^[-]+$|^[=]+$/
-            unless prev_line.nil?
-              @headers.push prev_line.chomp
-            end
-          end
+          sharp_header(line)
+          link(line, index, repo_root)
+          under_line_header(line, prev_line)
           prev_line = line
         end
       end
     end
 
-    def attribute(line, index, repo_root)
+    def under_line_header(line, prev_line)
+      if line =~ /^[-]+$|^[=]+$/
+        unless prev_line.nil?
+          @headers.push prev_line.chomp
+        end
+      end
+    end
+
+    def sharp_header(line)
       if line =~ heading_pattern # capture sharp header part
         header = Regexp.last_match[:header].downcase.rstrip.gsub(/\s+/," ")
         @headers.push header.gsub(" ", "-")
       end
+    end
+
+    def link(line, index, repo_root)
       line.scan link_pattern do |link| # capthure links path part
         @link_paths.push Path.new(@path, link[0].rstrip, index + 1, repo_root)
       end
